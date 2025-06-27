8b97eca8f28868d318ee139a352e87f138e07261
CesarMeier <cesar44135797@gmail.com>
2025-06-27 18:16:02 -0300

    agregamos requirements en estaticaController

--- src/Controller/EstaticaController.php
+++ src/Controller/EstaticaController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+#[Route('/sitio', name: 'app_sitio')]
 class EstaticaController extends AbstractController
 {
-    #[Route('/estatica/{pagina}', name: 'app_estatica')]
-    public function estaticaAction($pagina)
+    #[Route('/{pagina}', name: 'app_estatica', requirements:['pagina'=>'patrocinadores|licencia|condiciones|privacidad'])]
+    public function estaticaAction($pagina='patrocinadores')
     {
          return $this->render('estatica/'. $pagina .'.html.twig');
     }
